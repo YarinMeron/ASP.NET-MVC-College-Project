890959bd4d0bf189ae6cb3a94c2675fa10788b06
Yarin Meron <205702756@STUD.colman.ac.il>
2020-10-06 12:54:40 +0300

    Adding LoginModel

--- ShenkinStore/Models/LoginModel.cs
+++ ShenkinStore/Models/LoginModel.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ShenkinStore.Models
+namespace ShenkinBagsStore.Models
 {
     public class LoginModel
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "UserName is Requierd.")]
+        [Display(Name = "User Name")]
+
+        public string UserName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is Requierd.")]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
     }
 }
